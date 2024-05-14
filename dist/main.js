682a7801a0eb0f311a8ffd9a22a82e9aab30a437
Luis Balan <jlbalcz@gmail.com>
2024-05-14 01:20:32 -0500

    add database config info to gitignore

--- dist/main.js
+++ dist/main.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.get('/', (req, res) => {
     res.send("Hello, TypeScript with Express!");
 });
